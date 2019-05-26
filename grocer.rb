fdc2a9065dc9adde4a326939a4784c4ce407ac3a
github username <github email address>
2019-05-26 19:02:58 -0400

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,56 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  consolidated = {}
+  cart.each do |item|
+    item.each do |item_name, attributes|
+      if consolidated[item_name]
+        consolidated[item_name][:count] += 1
+      else
+        consolidated[item_name] = attributes
+        consolidated[item_name][:count] = 1
+      end
+    end
+  end
+  # binding.pry
+  consolidated
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  to_add = []
+  cart.each do |item, attributes|
+    coupons.each do |coupon|
+      to_add << coupon if coupon[:item] == item
+    end
+  end
+  
+  to_add.each do |coupon_info|
+    if cart[coupon_info[:item]][:count] >= coupon_info[:num]
+      if cart["#{coupon_info[:item]} W/COUPON"]
+        cart["#{coupon_info[:item]} W/COUPON"][:count] += 1
+        cart[coupon_info[:item]][:count] -= coupon_info[:num]
+      else
+        cart["#{coupon_info[:item]} W/COUPON"] = {
+          :price => coupon_info[:cost],
+          :clearance => cart[coupon_info[:item]][:clearance],
+          :count => 1
+        }
+        cart[coupon_info[:item]][:count] -= coupon_info[:num]
+      end
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each { |item, attributes| attributes[:price] = (attributes[:price] *= 0.8).round(2) if attributes[:clearance] }
 end
 
 def checkout(cart, coupons)
-  # code here
+  cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  total = 0
+
+  cart.each { |item, attributes| total += (attributes[:price] * attributes[:count]) }
+
+  total > 100 ? (total *= 0.9).round(2) : total
 end
